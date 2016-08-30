7a4e39d5ea0c9f392ab97966d14fee913da62625
Partoo Hwong <partoo@163.com>
2016-08-31 07:03:34 +0800

    init

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="menu-links">
+    <a v-link="{path:'/'}">HOME</a>
+    <a v-link="{path:'/cart'}">VIEW CART ({{ count }})</a>
+  </div>
+</template>
+
+
+
+<style>
+.menu-links a {
+  display: inline-block;
+  text-decoration: none;
+  color: #555;
+  margin-right: 30px;
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div v-for="p in products" class="product">
+      <a v-link="{name:'product', params:{id:p.id}}" class="title">{{ p.title }}</a>
+      <span class="price">{{ p.price | currency }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {getAllProducts, addToCart} from '../vuex/products/actions'
+  export default {
+    vuex: {
+      getters: {
+        products: ({products}) => products.all
+      },
+      actions: {
+        getAllProducts,
+        addToCart
+      }
+    },
+    created () {
+      this.getAllProducts()
+    }
+  }
+</script>
